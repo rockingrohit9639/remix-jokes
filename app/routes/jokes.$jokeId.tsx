e429b46d1de96110315c7e39a60d236cd2546e5c
= <rohitbhatwara1@gmail.com>
2023-12-31 09:53:52 +0530

    feat(jokes): showing joke by id and a random joke

--- app/routes/jokes.$jokeId.tsx
+++ app/routes/jokes.$jokeId.tsx
@@ -1,11 +1,32 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const jokeId = params.jokeId;
+  if (!jokeId) {
+    throw new Error("Joke id not found");
+  }
+
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+
+  if (!joke) {
+    throw new Error("Joke id not found");
+  }
+
+  return json({ joke });
+};
+
 export default function JokeRoute() {
+  const { joke } = useLoaderData<typeof loader>();
+
   return (
     <div>
       <p>Here is your hilarious joke:</p>
-      <p>
-        Why don&apos;t you find hippopotamuses hiding in trees? They are really
-        good at it.
-      </p>
+      <p>{joke.content}</p>
+      <Link to=".">&quot;{joke.name}&quot; Permalink</Link>
     </div>
   );
 }
