e440144813df5c7dca2c238cd3a0bbc0b11d1ca7
= <rohitbhatwara1@gmail.com>
2023-12-31 10:55:11 +0530

    feat(auth): add feature to register a new user

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -107,3 +107,11 @@ export async function logout(request: Request) {
     },
   });
 }
+
+export async function register({ password, username }: LoginForm) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = await db.user.create({
+    data: { passwordHash, username },
+  });
+  return { id: user.id, username };
+}
